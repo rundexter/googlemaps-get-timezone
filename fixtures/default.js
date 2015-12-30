9d52cf42ae668a7ec00685a651ce7f7233a55b35
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-30 13:03:21 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -62,8 +62,8 @@ module.exports = _.merge({
              * You should update this section with some test input for testing your module
              */
             input: {
-                //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                'location': '39.6034810,-119.6822510',
+                'timestamp': 1331161200
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+var _ = require('lodash'),
+    util = require('./util.js');
+
+var request = require('request').defaults({
+    baseUrl: 'https://maps.googleapis.com/maps/api/'
+});
+
+var pickInputs = {
+        'location': { key: 'location', validate: { req: true } },
+        'timestamp': { key: 'timestamp', validate: { req: true } },
+        'language': 'language'
+    },
+
+    pickOutputs = {
+        dstOffset: 'dstOffset',
+        rawOffset: 'rawOffset',
+        timeZoneId: 'timeZoneId',
+        timeZoneName: 'timeZoneName',
+        status: 'status'
+    };
+
 module.exports = {
+
+    /**
+     * Get auth data.
+     *
+     * @param step
+     * @param dexter
+     * @returns {*}
+     */
+    authOptions: function (step, dexter) {
+        if (dexter.environment('google_server_key'))
+            return dexter.environment('google_server_key');
+        else
+            return false;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +42,34 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var authKey = this.authOptions(step, dexter),
+            inputs = util.pickInputs(step, pickInputs),
+            uriLink = 'timezone/json';
+        var validateErrors = util.checkValidateErrors(inputs, pickInputs);
+
+        if (!authKey)
+            return this.fail('A [google_server_key] environment variable need for this module.');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        //send API request
+        request.get({
+            uri: uriLink,
+            qs: _.merge({ key: authKey }, inputs),
+            json: true
+        }, function (error, response, body) {
+            if (error)
+                this.fail(error);
+
+            else if (response && _.parseInt(response.statusCode) !== 200)
+                this.fail(body);
+
+            else if (body && body.error_message)
+                this.fail(body.error_message);
+
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
